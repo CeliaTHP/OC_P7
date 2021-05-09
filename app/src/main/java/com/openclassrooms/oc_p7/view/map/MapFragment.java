4dba063b62fdaf5182c38349f4325f0038b353a8
CeliaTHP <Tibiza121>
2021-05-09 03:16:44 +0200

    - Get and display nearest places

--- app/src/main/java/com/openclassrooms/oc_p7/view/map/MapFragment.java
+++ app/src/main/java/com/openclassrooms/oc_p7/view/map/MapFragment.java
@@ -1,6 +1,9 @@
 package com.openclassrooms.oc_p7.view.map;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,24 +12,36 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.firebase.auth.FirebaseAuth;
-import com.openclassrooms.oc_p7.R;
 import com.openclassrooms.oc_p7.databinding.FragmentMapBinding;
 import com.openclassrooms.oc_p7.view.LoginActivity;
 import com.openclassrooms.oc_p7.view_model.LoginViewModel;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
@@ -35,9 +50,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private MapViewModel mapViewModel;
     private FragmentMapBinding fragmentMapBinding;
     private MapView mapView;
+    private GoogleMap map;
 
 
     private LoginViewModel loginViewModel;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -62,13 +79,58 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         // mapViewModel.initMap(fragmentMapBinding.googleMap);
 
         initListeners();
-        initPlaces();
+        checkAndRequestPermissions();
 
     }
 
-    public void initPlaces() {
-        Places.initialize(getContext(), String.valueOf(R.string.google_api_key));
+    public void initPlaces(GoogleMap googleMap) {
+
+        getCurrentPlace();
+        Places.initialize(getContext(), "AIzaSyAfA5LPuDm4ZaZzcifGry_RhEPLjmSi5N4");
         PlacesClient placesClient = Places.createClient(getContext());
+
+        // Use fields to define the data types to return.
+        List<Place.Field> placeFieldsNames = Collections.singletonList(Place.Field.LAT_LNG);
+
+        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.newInstance(placeFieldsNames);
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
+            placeResponse.addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    FindCurrentPlaceResponse response = task.getResult();
+                    for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+
+                        Log.d(TAG, placeLikelihood.toString());
+
+                        googleMap.addMarker(new MarkerOptions().position(placeLikelihood.getPlace().getLatLng()).title(placeLikelihood.getPlace().getName()));
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(placeLikelihood.getPlace().getLatLng()));
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(placeLikelihood.getPlace().getLatLng(), 15));
+
+                    }
+                } else {
+                    Exception exception = task.getException();
+                    if (exception instanceof ApiException) {
+                        ApiException apiException = (ApiException) exception;
+                        Log.d(TAG, "PLACE NOT FOUND : " + apiException.getMessage());
+                    }
+                }
+
+            });
+        } else {
+            //ask permissions
+        }
+    }
+
+    private void getCurrentPlace() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+
+            Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+        }
+
     }
 
     public void initListeners() {
@@ -157,9 +219,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         LatLng paris = new LatLng(48.86306560056864, 2.2962409807179216);
         googleMap.addMarker(new MarkerOptions().position(paris).title("Paris"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(paris, 14));
+
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(@NonNull LatLng latLng) {
+                /*
                 MarkerOptions markerOptions = new MarkerOptions();
                 markerOptions.position(latLng);
                 markerOptions.title(latLng.latitude + " : " + latLng.longitude);
@@ -167,13 +232,24 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
                 googleMap.addMarker(markerOptions);
 
+                 */
+
             }
         });
+        initPlaces(googleMap);
     }
 
-    private void initLocation() {
-        // ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.RE)
+    private void checkAndRequestPermissions() {
+        Log.d(TAG, "FINE LOCATION PERMISSION : " + ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION));
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 19);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "Code : " + requestCode + " Permissions : " + Arrays.toString(permissions) + " result :" + Arrays.toString(grantResults));
 
     }
+
 
 }
